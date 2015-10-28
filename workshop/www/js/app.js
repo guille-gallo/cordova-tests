435a200e231716e3bbcb18c8cf161e2777d6e628
Guillermo Gallo <guillermo.gallo@globant.com>
2015-10-28 10:42:56 -0300

    first commit

---
+++ workshop/www/js/app.js
@@ -0,0 +1,36 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+	HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+	EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+	EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+	var slider = new PageSlider($('body'));
+	
+	
+	var service = new EmployeeService();
+	
+	service.initialize().done(function () {
+		router.addRoute('', function() {
+		  slider.slidePage(new HomeView(service).render().$el);
+		});
+
+		router.addRoute('employees/:id', function(id) {
+		  service.findById(parseInt(id)).done(function(employee) {
+			  slider.slidePage(new EmployeeView(employee).render().$el);
+		  });
+		});
+
+	  router.start();
+	});
+
+    /* --------------------------------- Event Registration -------------------------------- */
+    //$('.search-key').on('keyup', findByName);
+    /*$('.help-btn').on('click', function() {
+        alert("Employee Directory v3.4");
+    });*/
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+
+	
+}());
